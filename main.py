5a8b8d82df0ef33a860a2b93dd0acc678ac8a5e1
danilooxz <juniordanilo538@gmail.com>
2023-03-29 22:45:14 -0300

    lista5

---
+++ main.py
@@ -0,0 +1,8 @@
+area = float(input("Informe a área da casa: "))
+
+qntdeRejunte = area / 3
+qntdeArgamassa = area / 5
+
+print(f"A quantidade de rejunte necessaria é {qntdeRejunte:.1f} kg")
+
+print(f"A quantidade de argamassa necessaria é {qntdeArgamassa:.1f} kg")
